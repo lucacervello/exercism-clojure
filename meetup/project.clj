3dc893ed8659aa403a972043f19cad7f3605e97e
lucacervello <luca.cervello@gmail.com>
2016-10-13 19:48:16 +0200

    Remove dead code.

--- meetup/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject meetup "0.1.0-SNAPSHOT"
-  :description "meetup exercise."
-  :url "https://github.com/exercism/xclojure/tree/master/meetup"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.12.0"]])
