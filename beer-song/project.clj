a049e89aeaf6e577cf9e4c072708490c9de523dc
lucacervello <luca.cervello@gmail.com>
2016-10-14 11:18:21 +0200

    cleanup beer-song

--- beer-song/project.clj
+++ beer-song/project.clj
@@ -1,4 +1,4 @@
 (defproject beer-song "0.1.0-SNAPSHOT"
   :description "beer-song exercise."
   :url "https://github.com/exercism/xclojure/tree/master/beer"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
