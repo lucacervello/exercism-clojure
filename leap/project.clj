3e635652557003c06ad54196187adf6c84714412
lucacervello <luca.cervello@gmail.com>
2016-10-14 11:26:14 +0200

    cleanup leap

--- leap/project.clj
+++ leap/project.clj
@@ -1,6 +1,4 @@
 (defproject leap "0.1.0-SNAPSHOT"
   :description "leap exercise."
   :url "https://github.com/exercism/xclojure/tree/master/leap"
-  :source-paths [""]
-  :test-paths [""]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
